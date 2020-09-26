e5a1a2d994170973ee7cb533d96e486243956d8f
Julio Cesar Urbano Vasconcelos Filho <julio.vasconcelos@academico.ifpb.edu.br>
2020-09-26 19:12:46 -0300

    Setup Inicial

---
+++ src/main/java/com/imageFactory/ImageFactory/ImageFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.imageFactory.ImageFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageFactoryApplication.class, args);
+	}
+
+}
